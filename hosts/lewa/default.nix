0fb81302a075de81c9f8837b8899ad12f0dbffff
Caleb Schmucker <caleb@lewa.local>
2023-03-03 22:59:11 -0800

    change homebrew options

--- hosts/lewa/default.nix
+++ hosts/lewa/default.nix
@@ -4,30 +4,30 @@
 
   # Make sure the nix daemon always runs
   services.nix-daemon.enable = true;
-programs.zsh.enable = true;
-system.defaults.dock.autohide = true;
-home-manager.useGlobalPkgs = true;
-home-manager.useUserPackages = true;
-home-manager.users.caleb = { pkgs, ... }: {
-  home.stateVersion = "22.05";
-  programs.tmux = { # my tmux configuration, for example
-    enable = true;
-    keyMode = "vi";
-    clock24 = true;
-    historyLimit = 10000;
-    plugins = with pkgs.tmuxPlugins; [
-      vim-tmux-navigator
-      gruvbox
-    ];
-    extraConfig = ''
-      new-session -s main
-      bind-key -n C-a send-prefix
-    '';
+  programs.zsh.enable = true;
+  system.defaults.dock.autohide = true;
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.users.caleb = { pkgs, ... }: {
+    home.stateVersion = "22.05";
+    programs.tmux = { # my tmux configuration, for example
+      enable = true;
+      keyMode = "vi";
+      clock24 = true;
+      historyLimit = 10000;
+      plugins = with pkgs.tmuxPlugins; [
+        vim-tmux-navigator
+        gruvbox
+      ];
+      extraConfig = ''
+        new-session -s main
+        bind-key -n C-a send-prefix
+      '';
+    };
   };
-};
 homebrew = {
-  onActivation.enable = true;
-  onActivation.autoUpdate = true;
+  enable = true;
+  autoUpdate = true;
   # updates homebrew packages on activation,
   # can make darwin-rebuild much slower (otherwise i'd forget to do it ever though)
   casks = [
